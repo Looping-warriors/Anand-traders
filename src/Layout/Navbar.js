1f2545142385d48ac86fe90886f05404fc25a34b
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2024-03-09 09:52:06 +0530

    navbar designed as fixed

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,28 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../Assets/anand-logo.jpg";
 import "../css/Navbar.css";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className="Navbar">
-      <div>
-        <img className="nav-image" src={logo}></img>
-        <span className="nav-logo">Anand Traders</span>
-      </div>
-      <div className={`nav-items ${isOpen && "open"}`}>
-        <Link to="/home">Home</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/process">Process</Link>
-        <Link to="/products">Products</Link>
-        <Link to="/contact">Contact Us</Link>
-      </div>
-      <div
-        className={`nav-toggle ${isOpen && "open"}`}
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <div className="bar"></div>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div className={`Navbar ${scrolled && "scrollnav"}`}>
+        <div className='logo'>
+          <img
+            className='nav-image'
+            src={logo}
+          ></img>
+          <span className='nav-logo'>
+            <span >Anand</span> Traders
+          </span>
+        </div>
+        <div className={`nav-items ${isOpen && "open"}`}>
+          <Link to='/home'>Home</Link>
+          <Link to='/about'>About Us</Link>
+          <Link to='/process'>Process</Link>
+          <Link to='/products'>Products</Link>
+          <Link to='/contact'>Contact Us</Link>
+        </div>
+        <div
+          className={`nav-toggle ${isOpen && "open"}`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <div className='bar'></div>
+        </div>
       </div>
     </div>
   );
