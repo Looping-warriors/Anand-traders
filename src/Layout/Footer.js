bb2395786e1b0de325b46283b57cef7ca9073fcf
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2024-03-09 09:49:50 +0530

    footer designed as new

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -9,27 +9,27 @@ import { Link } from "react-router-dom";
 import "../css/Footer.css";
 function Footer() {
   return (
-    <div className="footer-main">
-      <div className="Footer">
-        <div className="footer-left-one">
-          <h4 className="footer-title">Anand Traders</h4>
-          <p className="footer-paragraph">
+    <div className='footer-main'>
+      <div className='Footer'>
+        <div className='footer-left-one'>
+          <h4 className='footer-title'>Anand Traders</h4>
+          <p className='footer-paragraph'>
             Anand Traders Located at Karur is one of the major Textiles
             manufacturing and exporter especially home furnishing textiles in
             South India.
           </p>
         </div>
-        <div className="footer-left">
-          <h4 className="footer-title">Address</h4>
-          <div className="footer-right-one">
-            <p className="footer-para">Anand Traders</p>
-            <p className="footer-para">
+        <div className='footer-left'>
+          <h4 className='footer-title'>Address</h4>
+          <div className='footer-right-one'>
+            <p className='footer-para'>Anand Traders</p>
+            <p className='footer-para'>
               10-A Valluvar Street , Karur – 639001.
             </p>
-            <p className="footer-para">Tamil Nadu , India</p>
+            <p className='footer-para'>Tamil Nadu , India</p>
           </div>
         </div>
-        <div className="footer-middle">
+        {/* <div className="footer-middle">
           <h4 className="footer-title">Company</h4>
           <ul>
             <li>
@@ -48,33 +48,36 @@ function Footer() {
               <Link to="/contact">Contact Us</Link>
             </li>
           </ul>
-        </div>
-        <div className="footer-middle-two">
-          <h4 className="footer-title">Location</h4>
+        </div> */}
+        <div className='footer-middle-two'>
+          <h4 className='footer-title'>Location</h4>
           <iframe
-            class="gmap_iframe"
-            frameborder="0"
-            scrolling="no"
-            marginheight="0"
-            marginwidth="0"
-            src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=10-A Valluvar Street, Karur – 639001, Tamilnadu , India.&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+            class='gmap_iframe'
+            frameborder='0'
+            scrolling='no'
+            marginheight='0'
+            marginwidth='0'
+            src='https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=10-A Valluvar Street, Karur – 639001, Tamilnadu , India.&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'
           ></iframe>
         </div>
       </div>
-      <div className="footer-bottom">
-        <hr className="horizontal-line" />
-        <div className="social-links">
+      <div className='footer-bottom'>
+        <hr className='horizontal-line' />
+        <div className='social-links'>
           <div>
-            <AiFillFacebook className="social-icons" />
-            <AiFillTwitterCircle className="social-icons" />
-            <AiFillInstagram className="social-icons" />
-            <AiFillLinkedin className="social-icons" />
-            <AiFillGoogleCircle className="social-icons" />
+            <AiFillFacebook className='social-icons' />
+            <AiFillTwitterCircle className='social-icons' />
+            <AiFillInstagram className='social-icons' />
+            <AiFillLinkedin className='social-icons' />
+            <AiFillGoogleCircle className='social-icons' />
           </div>
         </div>
-        <p className="copyright">
+        <p className='copyright'>
           @Copyright. All rights reserved. Designed by
-          <a href="http://interdeccaan.in/" target="-blank">
+          <a
+            href='http://interdeccaan.in/'
+            target='-blank'
+          >
             {" "}
             Inter Deccaan Solutions
           </a>
