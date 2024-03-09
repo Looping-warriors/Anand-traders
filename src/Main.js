033c744a6594e30b515584e2585c03c6b04604a3
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2024-03-09 09:46:57 +0530

    structure changed

--- src/Main.js
+++ src/Main.js
@@ -2,24 +2,42 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "../src/Layout/Navbar";
 import Footer from "../src/Layout/Footer";
-import Products from "./Components/Products";
-import About from "./Components/About";
-import Home from "./Components/Home";
-import Process from "./Components/Process";
-import Contact from "./Components/Contact";
+import Products from "./Components/Pages/Products";
+import About from "./Components/Pages/About";
+import Home from "./Components/Pages/Home";
+import Process from "./Components/Pages/Process";
+import Contact from "./Components/Pages/Contact";
 function Main() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
+          <Route
+            path='/'
+            element={<Home />}
+          />
+          <Route
+            path='/home'
+            element={<Home />}
+          />
 
-          <Route path="/about" element={<About />} />
-          <Route path="/process" element={<Process />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route
+            path='/about'
+            element={<About />}
+          />
+          <Route
+            path='/process'
+            element={<Process />}
+          />
+          <Route
+            path='/products'
+            element={<Products />}
+          />
+          <Route
+            path='/contact'
+            element={<Contact />}
+          />
         </Routes>
         <Footer />
       </BrowserRouter>
