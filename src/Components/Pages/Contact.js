e8a31031a7f0c888c2e80f970bb4333493f46682
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2024-03-10 14:28:22 +0530

    animation added

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,48 +1,73 @@
 import React from "react";
 import "../../css/Contact.css";
+import { motion } from "framer-motion";
 
 function Contact() {
   window.scrollTo(0, 0);
 
   return (
-    <div className="contact">
-      <div className="contact-left-flex">
-        <div className="enquiry-form">
-          <div className="enquiry">
+    <div className='contact'>
+      <motion.div
+        initial={{ position: "relative", right: 200, opacity: 0 }}
+        whileInView={{
+          position: "relative",
+          right: 0,
+          opacity: 1,
+          transition: { duration: 0.5, ease: "linear" },
+        }}
+        viewport={{ once: true }}
+        className='contact-left-flex'
+      >
+        <form className='enquiry-form'>
+          <div className='enquiry'>
             <h3>For Enquiry</h3>
-            <div className="input-div">
+            <div className='input-div'>
               <label>Full Name</label>
               <br />
-              <input type="text" placeholder="Your Name"></input>
+              <input
+                type='text'
+                placeholder='Your Name'
+              ></input>
             </div>
-            <div className="input-div">
+            <div className='input-div'>
               <label>Mail Address</label>
               <br />
-              <input type="mail" placeholder="Your Mail"></input>
+              <input
+                type='mail'
+                placeholder='Your Mail'
+              ></input>
             </div>
-            <div className="input-div">
+            <div className='input-div'>
               <label>Enquiry</label>
               <br />
               <textarea
-                className="enquiry-desc"
-                type="text"
-                placeholder="Your Enquiry"
+                className='enquiry-desc'
+                type='text'
+                placeholder='Your Enquiry'
               ></textarea>
             </div>
-            <div className="submit-btn">
+            <div className='submit-btn'>
               <button>Submit</button>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="contact-right-flex">
-        <div className="contact-right-flex-content">
+        </form>
+      </motion.div>
+      <motion.div
+        initial={{ position: "relative", left: 200, opacity: 0 }}
+        whileInView={{
+          position: "relative",
+          left: 0,
+          opacity: 1,
+          transition: { duration: 0.5, ease: "linear" },
+        }}
+        viewport={{ once: true }} className='contact-right-flex'>
+        <div className='contact-right-flex-content'>
           <h3>Contact Us</h3>
-          <div className="contact-section">
+          <div className='contact-section'>
             <h4>Anand Traders</h4>
             <p>10-A Valluvar Street, Karur – 639001, Tamilnadu , India.</p>
           </div>
-          <div className="contact-section">
+          <div className='contact-section'>
             <h4>Phone Number</h4>
             <p>Mr. R.Selvan - +91-9843056449</p>
             <p>Mr. P.R.Anand - +91-9443268449</p>
@@ -51,7 +76,7 @@ function Contact() {
           <p>selvan@selvantex.com</p>
           <p>anand@selvantex.com</p>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
